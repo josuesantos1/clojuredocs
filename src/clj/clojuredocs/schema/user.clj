6d6589ca56107e9cbb6f8b239558d7744e2f2eed
josue santos <josue.santos.dev@gmail.com>
2023-04-21 12:20:34 -0300

    [schema] example, namespace, note, user, var

---
+++ src/clj/clojuredocs/schema/user.clj
@@ -0,0 +1,18 @@
+(ns clojuredocs.schema.user)
+
+(def user
+  [{:db/ident       :user/id
+    :db/valueType   :db.type/uuid
+    :db/cardinality :db.cardinality/one
+    :db/unique      :db.unique/identity}
+   {:db/ident       :user/name
+    :db/valueType   :db.type/ref
+    :db/cardinality :db.cardinality/one}
+   {:db/ident       :user/token
+    :db/valueType   :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/doc         "token from gitlab or github"}
+   {:db/ident       :user/nickname
+    :db/valueType   :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/doc         "nickname from gitlab or github"}])
