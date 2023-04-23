e1c25aba11f1002eb641d54870f7958a858ec427
josue santos <josue.santos.dev@gmail.com>
2023-04-23 15:35:32 -0300

    use reframe to receive data

--- src/cljs/clojuredocs/page/vars.cljs
+++ src/cljs/clojuredocs/page/vars.cljs
@@ -1,57 +1,51 @@
 (ns clojuredocs.page.vars
   (:require
+    [re-frame.core :as rf] 
     [clojuredocs.components.sidebar :refer [sidebar]]))
 
-
-(def a
-  {:added "1.0",
-   :name 'map,
-   :file "clojure/core.clj",
-   :arglists "([f] [f coll] [f c1 c2] [f c1 c2 c3] [f c1 c2 c3 & colls])"
-   :doc "Returns a lazy sequence consisting of the result of applying f to\n  the set of first items of each coll, followed by applying f to the\n  set of second items in each coll, until any one of the colls is\n  exhausted.  Any remaining items in other colls are ignored. Function\n  f should accept number-of-colls arguments. Returns a transducer when\n  no collection is provided."})
-
-
 (defn example
-  []
+  [data]
   [:div.my-6
    [:strong.is-size-4
     "Exemplos"]
-   (map (fn [_]
-          [:div.box
-           [:strong "John Doe"]
+   (map (fn [example]
+          [:div.box {:key (:id example)}
+           [:strong (:name example)]
            [:hr]
-           [:pre "(map inc [1 2 3 4 5])"]]) (range 5))]) ; TODO: make a pretty view
+           [:pre (:content example)]]) data)]) ; TODO: make a pretty view
 
 (defn notes
-  []
+  [data]
   [:div.my-6
    [:strong.is-size-4
     "Comentarios"]
-   (map (fn [_]
-          [:div.box
-           [:strong "John Doe"]
+   (map (fn [comment*]
+          [:div.box {:key (:id comment*)}
+           [:strong (:name comment*)]
            [:hr]
-           [:pre "lorem ipsum"]]) (range 5))])
+           [:pre "lorem ipsum"]]) data)])
 
 
 (defn var-page
   []
-  [:div.container
-   [:div.box.m-4.columns
-    [sidebar]
-    [:div.column.is-9
-     [:div.box
-      [:strong.title (:name a)]
-      [:hr]
-      [:div
-       [:div
-        [:strong.tag.is-info.is-light.mx-2 (:file a)]
-        [:strong.tag.is-info.is-light.mx-2 (:arglists a)]
-        [:strong.tag.is-info.is-light.mx-2 "adicionado em:" (:added a)]]
-       [:hr]
-       [:pre
-        (:doc a)]
-       [example]
-       [:hr]
-       [notes]]]]]])
+  (let [content @(rf/subscribe [:var/content])]
+    (println content)
+    [:div.container
+     [:div.box.m-4.columns
+      [sidebar]
+      [:div.column.is-9
+       [:div.box
+        [:strong.title (:name content)]
+        [:hr]
+        [:div
+         [:div
+          [:strong.tag.is-info.is-light.mx-2 (:file content)]
+          [:strong.tag.is-info.is-light.mx-2 (:arglists content)]
+          [:strong.tag.is-info.is-light.mx-2 "adicionado em:" (:added content)]]
+         [:hr]
+         [:pre
+          (:doc content)]
+         [example (:example content)]
+         [:hr]
+         [notes (:comment content)]]]]]]))
 
