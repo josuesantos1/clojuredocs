8bf18c7243f9931e3397bb4fe68bfdcca7bf5caf
josue santos <josue.santos.dev@gmail.com>
2023-04-24 21:43:13 -0300

    button search

--- src/cljs/clojuredocs/page/home.cljs
+++ src/cljs/clojuredocs/page/home.cljs
@@ -6,8 +6,10 @@
   [:div.container
    [:div.m-6
     [:div.box {:style {:height "50vh"}}
-     [:div.is-flex.is-justify-content-center.py-6 {:style {:width "100%"}} [:h1.title.is-1 "clojuredocs br"]]
-     [:input.input
-      {:placeholder "buscar por..."}]
-     [:div]]]])
+     [:div.is-flex.is-justify-content-center.py-6 {:style {:width "100%"}}
+      [:h1.title.is-1 "clojuredocs br"]]
+     [:div.is-flex
+      [:input.input.mx-2
+       {:placeholder "buscar por..."}]
+      [:button.button.is-link.is-light.mx-2 "buscar"]]]]])
 
